f1bd009b2ddd0c387015427aabff8cf69ec23375
Amit Kumar Yadav <ay6732716@gmail.com>
2025-12-07 20:12:15 +0530

    Update database.py

--- Backend/database.py
+++ Backend/database.py
@@ -7,5 +7,5 @@ load_dotenv()
 # Example: MONGO_URI = "mongodb://localhost:27017"
 MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
-db = client["blood_buddy"]
+db = client["bloodbuddy"]
 donor_collection = db["donors"]
